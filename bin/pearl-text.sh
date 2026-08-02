ddb235431de3afbcb35774d1f54ccf2b9cfc718b
Pradyu12 <pradyus431@gmail.com>
2026-08-02 22:01:30 +0530

    Fix TL pane: use bash --norc to skip .zshrc waifu_hud, show THE BLACK PEARL text

--- bin/pearl-text.sh
+++ bin/pearl-text.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Black Pearl TL pane: shows logo text, then drops to shell
 echo -e "\n\n        \033[38;2;232;181;75mTHE BLACK PEARL\033[0m\n\n        \033[2mA ship too famous to be forgotten\033[0m"
-exec zsh
+exec bash --norc --noprofile
